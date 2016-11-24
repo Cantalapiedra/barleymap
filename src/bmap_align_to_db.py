644aa244623c2c3216ab6a0cdaae6a6e2a2713c1
Cantalapiedra <cpcantalapiedra@gmail.co>
2016-11-24 10:16:22 +0100

    Remove bmap_align_ext_db.py

--- src/bmap_align_to_db.py
+++ src/bmap_align_to_db.py
@@ -16,25 +16,33 @@ import sys, os
 from optparse import OptionParser
 
 from barleymapcore.alignment.AlignmentFacade import AlignmentFacade
-from barleymapcore.utils.data_utils import read_paths, load_data
+from barleymapcore.utils.data_utils import read_paths
 from barleymapcore.alignment.Aligners import SELECTION_BEST_SCORE, SELECTION_NONE
+from barleymapcore.db.DatabasesConfig import REF_TYPE_BIG, REF_TYPE_STD, DatabasesConfig
 
 DEFAULT_THRES_ID = 98.0
 DEFAULT_THRES_COV = 95.0
+DEFAULT_QUERY_MODE = "auto"
+DEFAULT_N_THREADS = 1
+DEFAULT_REF_TYPE = REF_TYPE_STD
 
 DATABASES_CONF = "conf/databases.conf"
 
-def _print_parameters(fasta_path, db_name, query_type, \
+def _print_parameters(fasta_path, dbs, query_type, \
                       threshold_id, threshold_cov, best_score, \
-                      n_threads, hierarchical):
+                      n_threads, hierarchical, ref_type = None):
     sys.stderr.write("\nParameters:\n")
     sys.stderr.write("\tQuery fasta: "+fasta_path+"\n")
-    sys.stderr.write("\tDB name: "+db_name+"\n")
+    sys.stderr.write("\tDBs: "+dbs+"\n")
     sys.stderr.write("\tQuery type: "+query_type+"\n")
     sys.stderr.write("\tThresholds --> %id="+str(threshold_id)+" : %query_cov="+str(threshold_cov)+"\n")
     sys.stderr.write("\tN threads: "+str(n_threads)+"\n")
     sys.stderr.write("\tBest score filtering: "+str(best_score)+"\n")
     sys.stderr.write("\tHierarchical filtering: "+str(hierarchical)+"\n")
+    
+    if ref_type:
+        sys.stderr.write("\tReference type: "+str(ref_type)+"\n")
+    
     return
     
 def _print_paths(split_blast_path, blastn_app_path, gmap_app_path, gmapl_app_path, blastn_dbs_path, gmap_dbs_path):
@@ -56,7 +64,10 @@ __usage = "usage: bmap_align_to_db.py [OPTIONS] [FASTA_FILE]\n\n"+\
 optParser = OptionParser(__usage)
 
 optParser.add_option('--databases', action='store', dest='databases_param', type='string', \
-                     help='Comma delimited list of databases to align to (default all).')
+                     help='Comma delimited list of database names to align to (default all).')
+
+optParser.add_option('--databases-ids', action='store', dest='databases_ids_param', type='string', \
+                     help='Comma delimited list of database IDs to align to (default all).')
 
 optParser.add_option('--query-mode', action='store', dest='query_mode', type='string', \
                      help='Alignment software to use (default auto). '+\
@@ -80,8 +91,8 @@ optParser.add_option('--best-score', action='store', dest='best_score', type='st
 optParser.add_option('--hierarchical', action='store', dest='hierarchical', type='string', \
                      help='Whether use datasets recursively (yes) or independently (default no).')
 
-#optParser.add_option('--align-info', action='store', dest='align_info', type='string', \
-#                     help='Whether obtain query-target info (default no) or append alignment info (yes).')
+optParser.add_option('--ref-type', action='store', dest='ref_type', type='string', \
+                     help='Whether use GMAP (std) or GMAPL (big), when using --databases-ids only.')
 
 optParser.add_option('-v', '--verbose', action='store_true', dest='verbose', help='More information printed.')
 
@@ -110,28 +121,20 @@ blastn_dbs_path = config_path_dict["blastn_dbs_path"]
 gmap_dbs_path = config_path_dict["gmap_dbs_path"]
 
 # Query mode
-if options.query_mode:
-    query_mode = options.query_mode
-else:
-    query_mode = "auto"
+if options.query_mode: query_mode = options.query_mode
+else: query_mode = DEFAULT_QUERY_MODE
     
 # Threshold Identity
-if options.thres_id:
-    threshold_id = float(options.thres_id)
-else:
-    threshold_id = DEFAULT_THRES_ID
+if options.thres_id: threshold_id = float(options.thres_id)
+else: threshold_id = DEFAULT_THRES_ID
 
 # Threshold coverage
-if options.thres_cov:
-    threshold_cov = float(options.thres_cov)
-else:
-    threshold_cov = DEFAULT_THRES_COV
+if options.thres_cov: threshold_cov = float(options.thres_cov)
+else: threshold_cov = DEFAULT_THRES_COV
     
 # Num threads
-if options.n_threads:
-    n_threads = int(options.n_threads)
-else:
-    n_threads = 1
+if options.n_threads: n_threads = int(options.n_threads)
+else: n_threads = DEFAULT_N_THREADS
 
 ## Selection: show secondary alignments
 if options.best_score and options.best_score == "no":
@@ -154,6 +157,12 @@ if options.hierarchical and options.hierarchical == "yes":
 else:
     hierarchical = False
     hierarchical_param = "no"
+    
+# ref_type
+if options.ref_type and options.ref_type == REF_TYPE_BIG:
+    ref_type_param = REF_TYPE_BIG
+else:
+    ref_type_param = DEFAULT_REF_TYPE
 
 # Extra alignment information
 #if options.align_info and options.align_info == "yes":
@@ -173,12 +182,27 @@ if verbose_param:
 
 # Databases
 databases_conf_file = __app_path+DATABASES_CONF
-(databases_names, databases_ids) = load_data(databases_conf_file, users_list = options.databases_param,
-                                             verbose = verbose_param) # data_utils.load_data
+databases_config = DatabasesConfig(databases_conf_file, verbose_param)
+
+if options.databases_param: # or "all databases in conf"
+    databases_names = options.databases_param
+    databases_ids = databases_config.get_databases_ids(databases_names.strip().split(","))
+    #(databases_names, databases_ids) = load_data(databases_conf_file, users_list = options.databases_param,
+    #                                         verbose = verbose_param) # data_utils.load_data
+    
+elif options.databases_ids_param:
+    databases_ids = options.databases_ids_param.strip().split(",")
+    databases_names = ",".join(databases_config.get_databases_names(databases_ids))
+    
+    #(databases_names, databases_ids) = load_ids(databases_conf_file, users_list = options.databases_ids_param,
+    #                                         verbose = verbose_param) # data_utils.load_data
+else:
+    databases_ids = databases_config.get_databases().keys()
+    databases_names = ",".join(databases_config.get_databases_names(databases_ids))
 
 _print_parameters(query_fasta_path, databases_names, query_mode, \
                   threshold_id, threshold_cov, options.best_score, \
-                  n_threads, hierarchical)
+                  n_threads, hierarchical, ref_type_param)
 
 ########### MAIN
 sys.stderr.write("\n")
@@ -187,11 +211,11 @@ sys.stderr.write("Start\n")
 # Load configuration paths
 facade = AlignmentFacade(split_blast_path, blastn_app_path, gmap_app_path, \
                          blastn_dbs_path, gmap_dbs_path, gmapl_app_path, tmp_files_path,
-                        databases_conf_file, verbose = verbose_param)
+                        databases_config, verbose = verbose_param)
 # Perform alignments
 results = facade.perform_alignment(query_fasta_path, databases_ids, hierarchical, query_mode, \
                                    threshold_id, threshold_cov, n_threads, \
-                                   selection, best_score_filter)
+                                   selection, best_score_filter, ref_type_param)
 
 ########## Output
 sys.stderr.write("\n")
--- src/bmap_map.py
+++ src/bmap_map.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# barleymap_find_markers.py is part of Barleymap.
-# Copyright (C)  2013-2014  Carlos P Cantalapiedra.
+# bmap_map.py is part of Barleymap.
+# Copyright (C)  2016  Carlos P Cantalapiedra.
 # (terms of use can be found within the distributed LICENSE file).
 
 ############################################
-# This script allows the user to retrieve the map
-# positions from marker IDs.
+# This script allows to translate alignments to DBs (obtained with bmap_align_to_db)
+# into Map positions.
 ############################################
 
 import sys, os#, traceback
@@ -16,65 +16,35 @@ from optparse import OptionParser
 from output.OutputFacade import OutputFacade
 from barleymapcore.datasets.DatasetsFacade import DatasetsFacade, SELECTION_NONE, SELECTION_BEST_SCORE
 from barleymapcore.maps.MapMarkers import MapMarkers
+from barleymapcore.maps.MapsConfig import MapsConfig
 from barleymapcore.utils.data_utils import read_paths, load_data
 
-def _print_parameters(query_ids_path, dataset_name, db_name, genetic_map_name, \
-                      sort_param, multiple_param, best_score, hierarchical, merge_maps, \
-                      show_genes_param, show_markers_param, \
-                      load_annot_param, genes_extend_param, genes_window, \
-                      show_unmapped_param):
+DATABASES_CONF = "conf/databases.conf"
+DATASETS_CONF = "conf/datasets.conf"
+MAPS_CONF = "conf/maps.conf"
+
+def _print_parameters(genetic_map_name, dataset_name):
     sys.stderr.write("\nParameters:\n")
-    sys.stderr.write("\tIDs query file: "+query_ids_path+"\n")
-    sys.stderr.write("\tDatasets list: "+dataset_name+"\n")
-    sys.stderr.write("\tDatabases list: "+db_name+"\n")
-    sys.stderr.write("\tGenetic maps: "+genetic_map_name+"\n")
-    sys.stderr.write("\tSort: "+sort_param+"\n")
-    sys.stderr.write("\tShow multiples: "+str(multiple_param)+"\n")
-    sys.stderr.write("\tBest score filtering: "+str(best_score)+"\n")
-    sys.stderr.write("\tHierarchical filtering: "+str(hierarchical)+"\n")
-    sys.stderr.write("\tMerge maps: "+str(merge_maps)+"\n")
-    sys.stderr.write("\tShow genes: "+str(show_genes_param)+"\n")
-    sys.stderr.write("\tShow markers: "+str(show_markers_param)+"\n")
-    sys.stderr.write("\tLoad annotation: "+str(load_annot_param)+"\n")
-    sys.stderr.write("\tExtend genes search: "+str(genes_extend_param)+"\n")
-    sys.stderr.write("\tGenes window: "+str(genes_window)+"\n")
-    sys.stderr.write("\tShow unmapped: "+str(show_unmapped_param)+"\n")
+    sys.stderr.write("\tMap: "+genetic_map_name+"\n")
+    sys.stderr.write("\tDataset: "+dataset_name+"\n")
     
     return
     
-############# BARLEYMAP_FIND_MARKERS
+#############
 try:
     
     ## Argument parsing
-    __usage = "usage: barleymap_find_markers.py [OPTIONS] [IDs_FILE]"
+    __usage = "usage: bmap_map.py [OPTIONS] [IDs_FILE]"
     optParser = OptionParser(__usage)
     
-    optParser.add_option('--maps', action='store', dest='maps_param', type='string', help='Comma delimited list of Maps to show.')
-    optParser.add_option('--sort', action='store', dest='sort_param', type='string', help='Sort results by centimorgan (cm) or basepairs (bp).')
-    optParser.add_option('--show-multiples', action='store', dest='multiple_param', type='string', help='Whether show (yes) or not (no) IDs with multiple positions.')
-    
-    optParser.add_option('--best-score', action='store', dest='best_score', type='string', \
-                     help='Whether return secondary hits (no), best score hits for each database (db) or overall best score hits (yes; default).')
-    
-    optParser.add_option('--hierarchical', action='store', dest='hierarchical', type='string', help='Whether use datasets recursively (yes) or independently (no).')
-    optParser.add_option('--merge', action='store', dest='merge_maps', type='string', help='Whether merge results from the different Maps (yes) or not (no).')
-    optParser.add_option('--genes', action='store', dest='show_genes', type='string', help='Show genes on marker (marker), between markers (between) \
-                         or dont show (no). If --genes is active, --markers option is ignored.')
-    optParser.add_option('--markers', action='store', dest='show_markers', type='string', help='Show additional markers (yes) or not (no). Ignored if --genes active.')
-    optParser.add_option('--annot', action='store', dest='load_annot', type='string', help='Whether load annotation info for genes (yes) or not (no).')
-    optParser.add_option('--extend', action='store', dest='extend', type='string', help='Whether extend search for genes (yes) or not (no).')
-    optParser.add_option('--genes-window', action='store', dest='genes_window', type='string', help='Centimorgans or basepairs (depending on sort)\
-                         to extend the search for genes (--genes) or markers (--markers).')
-    optParser.add_option('--show-unmapped', action='store', dest='show_unmapped', type='string', help='Whether to output only maps (no), or unmapped results as well (yes).')
-    
+    optParser.add_option('--map', action='store', dest='map_param', type='string', help='Map.')
+    optParser.add_option('--dataset', action='store', dest='dataset_param', type='string', help='Dataset to map.')
     optParser.add_option('-v', '--verbose', action='store_true', dest='verbose', help='More information printed.')
     
     (options, arguments) = optParser.parse_args()
     
     if not arguments or len(arguments)==0:
         optParser.exit(0, "You may wish to run '-help' option.\n")
-    
-    query_ids_path = arguments[0] # THIS IS MANDATORY
     
     if options.verbose: verbose_param = True
     else: verbose_param = False
@@ -89,151 +59,47 @@ try:
     __app_path = config_path_dict["app_path"]
     
     # Datasets
-    datasets_conf_file = config_path_dict["app_path"]+"conf/datasets.conf"
-    (datasets_names, datasets_ids) = load_data(datasets_conf_file, verbose = verbose_param) # data_utils.load_datasets
-    
-    # Databases
-    databases_conf_file = __app_path+"conf/references.conf"
-    (databases_names, databases_ids) = load_data(databases_conf_file, verbose = verbose_param) # data_utils.load_data
+    datasets_conf_file = __app_path+DATASETS_CONF
+    (dataset_name, dataset_id) = load_name(datasets_conf_file,
+                                               user_name = options.dataset_param,
+                                               verbose = verbose_param) # data_utils.load_name
     
     # Genetic maps
-    maps_conf_file = __app_path+"conf/maps.conf"
-    (maps_names, maps_ids) = load_data(maps_conf_file, users_list = options.maps_param, verbose = verbose_param)
+    maps_conf_file = __app_path+MAPS_CONF
+    (map_name, map_id) = load_name(maps_conf_file,
+                                       user_name = options.map_param,
+                                       verbose = verbose_param)
     
-    ## Sort
-    if options.sort_param and options.sort_param == "bp":
-        sort_param = "bp"
-    else:
-        sort_param = "cm"
-    
-    ## Multiple
-    if options.multiple_param and options.multiple_param == "yes":
-        multiple_param = True
-        multiple_param_text = "yes"
-    else:
-        multiple_param = False
-        multiple_param_text = "no"
-        
-    ## Selection: show secondary alignments
-    if options.best_score and options.best_score == "yes":
-        selection = SELECTION_NONE # or SELECTION_BEST_SCORE, the results should be the same
-        best_score_filter = True
-    else:
-        if options.best_score == "db":
-            selection = SELECTION_BEST_SCORE
-            best_score_filter = False
-        else: # options.best_score == "no":
-            selection = SELECTION_NONE
-            best_score_filter = False
-    # selection is applied on alignment results to each database separately
-    # best_score_filter is applied on all the results from all the databases
-
-    ## Hierarchical
-    if options.hierarchical and options.hierarchical == "yes":
-        hierarchical = True
-        hierarchical_param = "yes"
-    else:
-        hierarchical = False
-        hierarchical_param = "no"
-    
-    ## Merge maps
-    if options.merge_maps and options.merge_maps == "yes":
-        merge_maps = True
-        merge_maps_param = "yes"
-    else:
-        merge_maps = False
-        merge_maps_param = "no"
-    
-    ## Show genes
-    if options.show_genes:
-        show_genes_param = options.show_genes
-        if options.show_genes == "no":
-            show_genes = False
-        else:
-            show_genes = True
-    else:
-        show_genes_param = "no"
-        show_genes = False
-    
-    ## Show markers
-    if options.show_markers:
-        show_markers_param = options.show_markers
-        if options.show_markers == "no":
-            show_markers = False
-        else:
-            show_markers = True
-    else:
-        show_markers_param = "no"
-        show_markers = False
-    
-    ## Annotation
-    if options.load_annot and options.load_annot == "yes":
-        load_annot = True
-        load_annot_param = "yes"
-    else:
-        load_annot = False
-        load_annot_param = "no"
-    
-    ## Extend genes shown, on marker or in the edges when between markers
-    if options.extend and options.extend == "yes":
-        genes_extend = True
-        genes_extend_param = "yes"
-    else:
-        genes_extend = False
-        genes_extend_param = "no"
-    
-    ## Genes window
-    if options.genes_window:
-        genes_window = float(options.genes_window)
-    else:
-        genes_window = 0.2
-    # genes_window_unit = "cM" ESTO LO HE CAMBIADO. TODO SE HARA POR EL SORT_PARAM
-    
-    ## Show unmapped
-    if options.show_unmapped and options.show_unmapped == "yes":
-        show_unmapped_param = "yes"
-        show_unmapped = True
-    else:
-        show_unmapped_param = "no"
-        show_unmapped = False
-    
-    if verbose_param: _print_parameters(query_ids_path, datasets_names, databases_names, maps_names, \
-                      sort_param, multiple_param_text, options.best_score, hierarchical_param, merge_maps, \
-                      show_genes_param, show_markers_param, \
-                      load_annot_param, genes_extend_param, genes_window, \
-                      show_unmapped_param)
+    if verbose_param: _print_parameters(map_name, dataset_name)
     
     ############################################################ MAIN
     if verbose_param: sys.stderr.write("\n")
     
     ############ ALIGNMENTS - DATASETS
     # Load configuration paths
+    
+    maps_config = MapsConfig(maps_conf_file, verbose = verbose_param)
+    map_config  = maps_config.get_map(map_ids)
+    map_databases = maps_config.get_map_db_list(map_config)
+    map_hierarchical = maps_config.get_map_is_hierarchical(map_config)
+    
+    # Perform alignments
     datasets_path = __app_path+config_path_dict["datasets_path"]
     facade = DatasetsFacade(datasets_conf_file, datasets_path, verbose = verbose_param)
-    # Perform alignments
-    results = facade.retrieve_ids(query_ids_path, datasets_ids, databases_ids, hierarchical, \
-                                  selection, best_score_filter)
+    results = facade.retrieve_dataset_alignments(dataset_id, map_databases, map_hierarchical)
     
-    unmapped = facade.get_alignment_unmapped()
+    unmapped = []#facade.get_alignment_unmapped()
     
     ############ MAPS
-    mapMarkers = MapMarkers(config_path_dict, maps_ids, verbose_param)
-    mapMarkers.create_genetic_maps(results, unmapped, databases_ids, sort_param, multiple_param, merge_maps)
-    
-    ############ OTHER MARKERS
-    if show_markers and not show_genes:
-        markers_dict = mapMarkers.enrich_with_markers(genes_extend, genes_window, genes_window, sort_param, \
-                                                      databases_ids, datasets_ids, datasets_conf_file, hierarchical, merge_maps, constrain_fine_mapping = False)
-    ########### GENES
-    if show_genes:
-        mapMarkers.enrich_with_genes(show_genes_param, load_annot, genes_extend, genes_window, genes_window, sort_param, constrain_fine_mapping = False)
+    mapMarkers = MapMarkers(config_path_dict, [map_id], verbose_param)
+    mapMarkers.create_genetic_maps(results, unmapped, map_databases)
     
     ############################################################ OUTPUT
     genetic_map_dict = mapMarkers.get_genetic_maps()
     
     outputPrinter = OutputFacade().get_plain_printer(sys.stdout, verbose = verbose_param)
     
-    outputPrinter.print_maps(outputPrinter, maps_ids, genetic_map_dict, show_genes, show_markers, show_unmapped, multiple_param_text, load_annot, show_headers = True)
+    outputPrinter.print_maps(outputPrinter, [map_id], genetic_map_dict, show_headers = True)
 
 except Exception as e:
     #traceback.print_exc(file=sys.stderr)
