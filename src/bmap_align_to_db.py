ef6b7e551c0250f3bbec0088736ee07fe9e45110
eead-csic-compbio <contrera@baobab.auladei.eead.csic.es>
2021-11-02 14:38:49 +0100

    added python2 to shebang

--- src/bmap_align_to_db.py
+++ src/bmap_align_to_db.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 # bmap_align_to_db.py is part of Barleymap.
--- src/bmap_config.py
+++ src/bmap_config.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 # check_config.py is part of Barleymap.
